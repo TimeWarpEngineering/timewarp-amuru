a5454962aab3f69b35726181f959259229710a8b
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2025-12-11 20:30:47 +0700

    Remove dead code after Ganda/exe migration to timewarp-ganda repo
    - Simplify ci-cd.yml from 334 to ~130 lines (remove exe build jobs)
    - Delete sync-configurable-files.yml (referenced non-existent Tools/FileSync/)
    - Delete exe/ directory (all 13 files exist in timewarp-ganda repo)
    - Remove exe/ path triggers from CI workflow
    
    Closes #61

--- exe/installer.cs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/dotnet --
-#:project ../Source/TimeWarp.Amuru/TimeWarp.Amuru.csproj
-
-using TimeWarp.Amuru;
-
-// Simple wrapper that calls the Amuru library's installer
-return await Installer.InstallUtilitiesAsync(args);
