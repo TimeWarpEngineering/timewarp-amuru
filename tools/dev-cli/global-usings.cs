baae85c9e2e2df156b5e322891e900e7e7cf73b4
Steven T. Cramer <steven.cramer@timewarp.enterprises>
2026-04-13 10:34:20 +0700

    fix: include global-usings.cs in dev-cli compilation and consolidate all usings there

--- tools/dev-cli/global-usings.cs
+++ tools/dev-cli/global-usings.cs
@@ -10,11 +10,15 @@ global using System.Linq;
 global using System.Runtime.CompilerServices;
 global using System.Runtime.InteropServices;
 global using System.Text;
+global using System.Text.Json;
 global using System.Threading;
 global using System.Threading.Tasks;
+global using System.Xml.Linq;
 
 global using TimeWarp.Nuru;
 global using static TimeWarp.Nuru.Unit;
 global using TimeWarp.Amuru;
 global using TimeWarp.Terminal;
 global using Microsoft.Extensions.DependencyInjection;
+
+global using static System.Console;
